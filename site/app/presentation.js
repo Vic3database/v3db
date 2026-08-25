e191bd8e8c7892440deba1af4fe240c84d9eff81
Vic3database <302398767+Vic3database@users.noreply.github.com>
2026-08-25 19:55:18 +0800

    fix: preserve complete incorporation scenario options

--- site/app/presentation.js
+++ site/app/presentation.js
@@ -332,6 +332,7 @@ function syncListSelection(attribute, previousKey, nextKey) {
 function commitCountrySelection(countryTag) {
   const previousTag = state.selectedTag;
   state.globalSearchColorRestoreTag = "";
+  if (previousTag !== countryTag) clearCountryIncorporationScenario();
   state.selectedTag = countryTag;
   state.detailKind = "country";
   replaceHash(selectionHashForCard("/country", "/country/" + encodeURIComponent(countryTag)));
